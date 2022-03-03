da904f9668e86a275055e24440e21ba58aae4db0
Madeline Garcia-Cabrera <vmgarcia.cabrera21@gmail.com>
2022-03-02 22:06:46 -0500

    models reaction

--- models/Thought.js
+++ models/Thought.js
@@ -2,16 +2,31 @@ const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
 const reactionSchema = new Schema({
-    
-
-    }
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxLength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+      },
+},
+    {
+    toJSON: {
+        getters: true,
+      }
+    } 
 );
-
-
-
-
-
-
 
 const ThoughtSchema = new Schema({
     thoughtText:{
@@ -30,5 +45,21 @@ const ThoughtSchema = new Schema({
         required: true
     },
     reactions:[reactionSchema]
-}
+},
+{
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
 );
+
+ThoughtSchema.virtual('reactionCount').get(function(){
+    return this.reactions.length;
+});
+
+const Thought = model('Thought',ThoughtSchema);
+
+
+module.exports = Thought;
